56b73f3828db381148a485e2543949dc61bf8f5e
Snexok <Snexok@users.noreply.github.com>
2024-10-12 16:34:04 +0000

    deploy: a1831a9bf52b084bb584baf5163bc7bc023d626a

--- assets/Main-65DTV6SY.js
+++
@@ -1,1 +0,0 @@
-import{u as s,r as e,j as t}from"./index-CQlShBf1.js";const n=()=>{const a=s();return e.useEffect(()=>{a("/login")},[]),t.jsx(t.Fragment,{})};export{n as default};
---
+++ assets/Main-Bn9Zdw_4.js
@@ -0,0 +1,1 @@
+import{u as s,r as e,j as t}from"./index-DESUTxUb.js";const n=()=>{const a=s();return e.useEffect(()=>{a("/login")},[]),t.jsx(t.Fragment,{})};export{n as default};
